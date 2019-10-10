d78dd9d1d217a1fecd67dd71bd2dfd8ab93db37b
ersin.sivaz@sbu.edu.tr <ersinsivaz@hotmail.com>
2019-10-10 17:01:21 +0300

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDZXD0vUNplszeDi6fvHuzkXs_pB7Q9Qcs',
+    authDomain: 'pepper-d2151.firebaseapp.com',
+    databaseURL: 'https://pepper-d2151.firebaseio.com',
+    projectId: 'pepper-d2151',
+    storageBucket: 'pepper-d2151.appspot.com',
+    messagingSenderId: '982392637383',
+    appId: "1:982392637383:web:252496a0a9b5f884a8fbe2"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDZXD0vUNplszeDi6fvHuzkXs_pB7Q9Qcs',
+    authDomain: 'pepper-d2151.firebaseapp.com',
+    databaseURL: 'https://pepper-d2151.firebaseio.com',
+    projectId: 'pepper-d2151',
+    storageBucket: 'pepper-d2151.appspot.com',
+    messagingSenderId: '982392637383',
+    appId: "1:982392637383:web:252496a0a9b5f884a8fbe2"
+  }
+};
